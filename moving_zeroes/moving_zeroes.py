7510fd75363e7c82a4977f55d5e519a584fce2f7
tmbern <tmbern42@gmail.com>
2020-06-17 14:15:40 -0600

    completed moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,10 +3,16 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    new_arr = []
+    for i in arr:
+        if i != 0:
+            new_arr.insert(0, i)
+        else:
+            new_arr.append(i)
+    return new_arr
 
-    pass
-
+    # could also try to remove 0 and append 0 for however many
+    # times there are 0 in the list. 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
