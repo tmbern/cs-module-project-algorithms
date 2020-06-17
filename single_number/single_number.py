71d10a9b343696ba4ef3f358c033eac310ccb033
tmbern <tmbern42@gmail.com>
2020-06-17 14:18:19 -0600

    refactored single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,13 +3,17 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    single_number = []
+    # single_number = []
+    # for i in arr:
+    #     if i in single_number:
+    #         single_number.remove(i)
+    #     else:
+    #         single_number.append(i)
+    # return single_number[0]
+
     for i in arr:
-        if i in single_number:
-            single_number.remove(i)
-        else:
-            single_number.append(i)
-    return single_number[0]
+        if arr.count(arr[i]) == 1:
+            return arr[i]
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
